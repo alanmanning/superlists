0bb45465d558096df9f4bc0e68500a072d60a403
Alan Manning <mannin2@phas.ubc.ca>
2017-10-08 11:29:49 -0700

    Added homepage view, tests for minimal html HttpResponse. All tests pass

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
+from django.http import HttpRequest
 
 # Create your tests here.
 
@@ -10,3 +11,13 @@ class HomePageTest(TestCase):
 	def test_root_url_resolves_to_homepage_view(self):
 		found = resolve('/')
 		self.assertEquals(found.func, home_page)
+
+	def test_home_page_view_returns_correct_html(self):
+		request = HttpRequest()
+		print('The default method for HttpRequest is: '+str(request.method))
+		response = home_page(request)
+		html = response.content.decode('utf8')
+		self.assertTrue(html.startswith('<html>'))
+		self.assertIn('<title>To-Do lists</title>',html)
+		self.assertTrue(html.endswith('</html>'))
+
