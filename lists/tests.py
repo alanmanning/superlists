73ed32283f6ec052368b337f212a892ee2afa462
Alan Manning <alanmanning@gmail.com>
2017-10-10 23:39:10 -0700

    Working on chromebook. Refactored homepage to use templates.

--- lists/tests.py
+++ lists/tests.py
@@ -18,6 +18,6 @@ class HomePageTest(TestCase):
 		response = home_page(request)
 		html = response.content.decode('utf8')
 		self.assertTrue(html.startswith('<html>'))
-		self.assertIn('<title>To-Do lists</title>',html)
+		self.assertIn('<title>To-Do Lists</title>',html)
 		self.assertTrue(html.endswith('</html>'))
 
