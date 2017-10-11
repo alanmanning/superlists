9935d01ecef1a3953a116f52b3523cba73feeef5
Alan Manning <alanmanning@gmail.com>
2017-10-10 23:58:37 -0700

    Added templates folder

--- lists/tests.py
+++ lists/tests.py
@@ -14,9 +14,9 @@ class HomePageTest(TestCase):
 		self.assertEquals(found.func, home_page)
 
 	def test_home_page_view_returns_correct_html(self):
-		request = HttpRequest()
-		response = home_page(request)
+		response = self.client.get('/')
 		html = response.content.decode('utf8')
 		expected_html = render_to_string('home.html')
 		self.assertEqual(html,expected_html)
+		self.assertTemplateUsed(response,'home.html')
 
