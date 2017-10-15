fc874f5bea82e74edb3aa5e8de159b700503b489
Alan Manning <alanmanning@gmail.com>
2017-10-15 16:16:57 -0700

    Converted tab indent to spaces. Ran first 'migrate' command. Simplified some units tests. Home page now iterates over list items.

--- lists/tests.py
+++ lists/tests.py
@@ -10,37 +10,46 @@ from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
 
-	def test_root_url_resolves_to_homepage_view(self):
-		found = resolve('/')
-		self.assertEquals(found.func, home_page)
+    def test_root_url_resolves_to_homepage_view(self):
+        found = resolve('/')
+        self.assertEquals(found.func, home_page)
 
-	def test_home_page_view_returns_correct_html(self):
-		response = self.client.get('/')
-		# html = response.content.decode('utf8')
-		# expected_html = render_to_string('home.html')
-		# self.assertEqual(html,expected_html)
-		self.assertTemplateUsed(response,'home.html')
+    def test_home_page_view_returns_correct_html(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response,'home.html')
 
-	def test_can_save_a_POST_request(self):
-		response = self.client.post('/',data={'item_text' : 'A new list item'})
-		html = response.content.decode('utf8')
-		self.assertIn('A new list item',html)
-		self.assertTemplateUsed(response,'home.html')
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/',data={'item_text' : 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'],'/')
 
-class ItemModelTest(TestCase):
+# class ItemModelTest(TestCase):
 
-	def test_saving_and_retreiving_items(self):
-		first_item = Item()
-		first_item.text = 'The first ever list item'
-		first_item.save()
+#   def test_saving_and_retreiving_items(self):
+#       first_item = Item()
+#       first_item.text = 'The first ever list item'
+#       first_item.save()
 
-		second_item = Item()
-		second_item.text = 'Second list item'
-		second_item.save()
+#       second_item = Item()
+#       second_item.text = 'Second list item'
+#       second_item.save()
 
-		saved_items = Item.objects.all()
-		self.assertEqual(saved_items.count(),2)
+#       saved_items = Item.objects.all()
+#       self.assertEqual(saved_items.count(),2)
 
-		self.assertEqual(saved_items[0].text,'The first ever list item')
-		self.assertEqual(saved_items[1].text,'Second list item')
+#       self.assertEqual(saved_items[0].text,'The first ever list item')
+#       self.assertEqual(saved_items[1].text,'Second list item')
+
+#   def test_can_save_a_POST_request(self):
+#       response = self.client.post('/',data={'item_text': 'A new list item'})
+
+#       self.assertEqual(Item.objects.count(),1)
+#       new_item = Item.objects.first()
+#       self.assertEqual(new_item.text, 'A new list item')
+
+
+
+#   def test_only_saves_items_when_necessary(self):
+#       self.client.get('/')
+#       self.assertEqual(Item.objects.count(),0)
 
--- lists/views.py
+++ lists/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from lists.models import Item
 
 # Create your views here.
 
 
 def home_page(request):
-	item = request.POST.get('item_text','')
-	print('POSTed item is: '+ item)
-	return render(request,'home.html',{
-			'new_item_text' : request.POST.get('item_text',''),
-		})
+    if request.method == 'POST':
+        print('Got a post request')
+        new_item_text = request.POST['item_text']
+        print('Got item text: '+new_item_text)
+        item = Item()
+        print('Made new item object')
+        item.text = new_item_text
+        print('set item text')
+        item.save()
+        print('Saved item in db')
+        # Item.objects.create(text=new_item_text)
+        # print('Made a new item and saved it in db')
+        return redirect('/')
+
+    items = Item.objects.all()
+    print(items)
+    return render(request,'home.html',{'list_items' : items})
